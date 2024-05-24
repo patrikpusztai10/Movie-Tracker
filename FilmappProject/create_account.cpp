12eb897831f2aa0e5b81395618f5180e1a36da7b
patrikpusztai10 <150380680+patrikpusztai10@users.noreply.github.com>
2024-05-24 19:44:27 +0300

    Update create_account.cpp

--- FilmappProject/create_account.cpp
+++ FilmappProject/create_account.cpp
@@ -5,29 +5,35 @@ User::User(std::string username,std::string email,std::string password)
     this->username=username;
     this->email=email;
     this->password=password;
-    numberofusers=0;
+
 }
+
 void User::create_account()
 {
-std::cout<<std::endl<<std::endl;
+    std::cout<<std::endl<<std::endl;
     std::string welcome_message="Create account";
     int spaces=(115-welcome_message.size())/2;
-std::string username,email,password;
-std::cout<<std::endl<<"Enter the following details:"<<std::endl;
-std::cout<<"Enter the username:";
-std::cin>>username;
-std::cout<<"Enter the email:";
-std::cin>>email;
-std::cout<<"Enter the pawssord:";
-std::cin>>password;
-User user{username,email,password};
-user.uprint();
+    std::string username,email,password;
+    std::cout<<std::endl<<"Enter the following details:"<<std::endl;
+    std::cout<<"Enter the username:";
+    std::cin>>username;
+    std::cout<<"Enter the email:";
+    std::cin>>email;
+    std::cout<<"Enter the pawssord:";
+    std::cin>>password;
+    std::shared_ptr<User> user = std::make_shared<User>(username, email, password);
+    users.push_back(user);
+    user->uprint();
 }
 void User::uprint()
-{  std::ofstream fout("accounts.csv", std::ios::app); // Append mode
-    if (fout.is_open()) {
+{
+    std::ofstream fout("accounts.csv", std::ios::app);
+    if (fout.is_open())
+    {
         fout << username << ',' << email << ',' << password << std::endl;
-    } else {
+    }
+    else
+    {
         std::cerr << "Unable to open file" << std::endl;
     }
 }
