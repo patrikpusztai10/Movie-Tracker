9315c9a2e5f4bdc07c72ae05d0b6ca9964c5b61d
patrikpusztai10 <150380680+patrikpusztai10@users.noreply.github.com>
2024-05-24 19:19:51 +0300

    Update header.h
    Am schimbat tot in clase

--- FilmappProject/header.h
+++ FilmappProject/header.h
@@ -1,14 +1,24 @@
 #pragma once
 #include <iostream>
 #include <fstream>
-struct User{
+class User{
+protected:
 std::string username;
 std::string email;
 std::string password;
+int numberofusers;
+public:
+User(std::string,std::string,std::string);
+void create_account();
+void uprint();
+void addUser();
 };
-struct Movie{
+void create_account();
+void uprint();
+class Movie{
+protected:
 std::string title;
 std::string genre;
 int rating;
 };
-void create_account();
+
